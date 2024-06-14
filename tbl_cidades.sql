402937bb98ba04fc23af2085c014f24a319206b2
enriquedsantos <enrique.d.santos@hotmail.com>
2024-06-14 16:05:31 -0300

    Add files via upload

---
+++ tbl_cidades.sql
@@ -0,0 +1,5 @@
+USE db_agencia_de_turismo;
+CREATE TABLE IF NOT EXISTS `tbl_cidades`(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
---
+++ tbl_turistas.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `db_agencia_de_turismo`.`tbl_turistas` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
